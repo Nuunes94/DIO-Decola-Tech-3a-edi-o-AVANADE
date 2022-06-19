c5ff51d8bf4a92ee78bffa164b895088595a2a61
Lucas <lucasalexandre07@hotmail.com>
2022-06-19 18:52:59 -0300

    Atualização

---
+++ JavaScript/Aula 5 - fucoes/script.js
@@ -0,0 +1,13 @@
+/* // Função sem parametro
+
+function funcao () {
+    console.log("Olá!");
+}
+funcao(); */
+
+// Função com parametro
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+mensagem('Olá','eu sou o Goku!');
---
+++ JavaScript/Aula 6 - aprofundando em funcoes/tipoDeFuncao.js
@@ -0,0 +1,20 @@
+// tipos de função
+
+// declarativa
+/* function funcao() {
+    console.log('Olá, eu sou o Goku!')
+}
+funcao(); */
+
+// expresão de funcão
+// com nomeação
+/* var funcao = function () {
+    console.log('Olá, eu de novo')
+}
+funcao(); */
+
+// arrow function
+/* var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+funcao(); */
